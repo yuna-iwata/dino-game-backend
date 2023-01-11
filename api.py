9f53681cbd2967133dad7d72b322386ae2d77451
Yuna Iwata <yunaiwata@Yunas-MacBook-Pro.local>
2023-01-11 22:24:59 +0000

    dino database inserted

--- api.py
+++ api.py
@@ -1,14 +1,14 @@
 import psycopg2
 import psycopg2.extras as pse  # We'll need this to convert SQL responses into dictionaries
 from flask import Flask, current_app, request, jsonify
-from flask_cors import CORS
+# from flask_cors import CORS
 
 app=Flask(__name__)
-CORS(app)
+# CORS(app)
 
 def get_db_connection():
     try:
-        conn = psycopg2.connect("dbname=czreijar user=czreijar password=TJ2StTuQIl2CoRoinQTwPxk8pBGfdf6t host=kandula.db.elephantsql.com port=5432")
+        conn = psycopg2.connect("dbname=dzmxageo user=dzmxageo password=aXwhLXYi3XWyQiLf3cEid5qHuj4oU6eL host=rogue.db.elephantsql.com port=5432")
         return conn
     except:
         print('Error Connecting to Database')
@@ -30,7 +30,25 @@ def db_select(query, parameters=()):
 
 @app.route("/", methods=['GET'])
 def index():
-    return 'Countries Application'
+    return 'Dino API'
+
+@app.route("/leaderboard", methods=['GET'])
+def get_leaderboard():
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor(cursor_factory=pse.RealDictCursor)
+
+        cur.execute("SELECT * from scores")
+
+        leaderboard_data = cur.fetchall()
+        cur.close()
+
+        return leaderboard_data, 200, {'Content-Type': 'application/json'}
+
+    except:
+        return 'Failed to fetch Data', 404
+
+
 
 @app.route("/countries/<string:name>", methods=['GET'])
 def get_countries(name):
